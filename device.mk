7b2b6f75b401e1c388eea7aa8d2850fc5ac5c3ae
rahul <rahul@aospa.co>
2024-04-23 13:06:24 +0100

    unicorn: Configure display timer overrides
    Change-Id: I5c7a85b3968cb8b1ece3300703f9d3c0151a8817

--- device.mk
+++ device.mk
@@ -31,6 +31,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.product.mod_device=unicorn
 
+# Display - timers
+PRODUCT_ODM_PROPERTIES += \
+    ro.surface_flinger.set_idle_timer_ms?=1000 \
+    ro.surface_flinger.set_touch_timer_ms?=200
+
 # Graphics
 PRODUCT_VENDOR_PROPERTIES += \
     ro.gfx.driver.1=com.qualcomm.qti.gpudrivers.taro.api31
