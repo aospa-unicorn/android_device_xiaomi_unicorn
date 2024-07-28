4cd0f75f45973c87c4332e3ae9aedab484c6552a
QKIvan <80185263+QKIvan@users.noreply.github.com>
2024-07-29 04:32:09 +0800

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -6,23 +6,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-function blob_fixup() {
-    case "${1}" in
-        vendor/etc/camera/pureView_parameter.xml)
-            sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
-            ;;
-        vendor/etc/camera/unicorn_enhance_motiontuning.xml|vendor/etc/camera/unicorn_motiontuning.xml)
-            sed -i 's/xml=version/xml version/g' "${2}"
-            ;;
-        vendor/etc/init/hw/init.batterysecret.rc)
-            sed -i 's/on charger/on property:init.svc.vendor.charger=running/g' "${2}"
-            ;;
-        vendor/lib/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so|vendor/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so)
-            sed -i 's/_ZN13DisplayConfig10ClientImpl13ClientImplGetENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE/_ZN13DisplayConfig10ClientImpl4InitENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0/g' "${2}"
-            ;;
-    esac
-}
-
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
