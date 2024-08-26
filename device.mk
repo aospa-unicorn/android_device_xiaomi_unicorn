788d891eb7da684140181c84b728cead40b419b8
RITEFANG <1721985272@qq.com>
2024-08-26 21:03:20 +0800

    unicorn: overlay: Optimize statusbar padding
    Change-Id: I0d0665a937ef454bb460f98a19764de121383ed8
    Signed-off-by: RITEFANG <1721985272@qq.com>

--- device.mk
+++ device.mk
@@ -47,7 +47,8 @@ PRODUCT_PACKAGES += \
     SettingsProviderResUnicorn \
     SettingsResUnicorn \
     SystemUIResUnicorn \
-    WifiResUnicorn
+    WifiResUnicorn \
+    UnicornSystemUIOverlay
 
 # NFC
 TARGET_NFC_SKU := unicorn
